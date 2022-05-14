c4d8610ecc86f9228d7d352eca867eafc338fa6e
GeneralD <yumejustice@gmail.com>
2022-05-15 01:41:12 +0900

    refactor: sort props in truffle-config.js

--- truffle-config.js
+++ truffle-config.js
@@ -1,22 +1,22 @@
 module.exports = {
-  networks: {
-    development: {
-      host: "127.0.0.1",
-      port: 7545,
-      network_id: "*", // Match any network id
-      gas: 5000000
-    }
-  },
-  contracts_build_directory: "./src/artifacts/",
   compilers: {
     solc: {
       version: "0.8.13",
       settings: {
         optimizer: {
-          enabled: true, // Default: false
-          runs: 200      // Default: 200
+          enabled: true,
+          runs: 200
         },
       }
+    }
+  },
+  contracts_build_directory: "./src/artifacts/", // to use ABI in frontend (react)
+  networks: {
+    development: {
+      host: "127.0.0.1",
+      port: 7545,
+      network_id: "*",
+      gas: 5_000_000
     }
   }
 }
